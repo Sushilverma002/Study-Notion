f4714eb8c29dbe1fbe4c526c6773b823484c484e
Sushilverma002 <sushilvarma9911@gmail.com>
2024-07-17 08:00:47 +0530

    17th july 2024

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,24 +1,51 @@
 import React from "react";
 import { NavbarLinks } from "../../data/navbar-links.js";
 import Logo from "../../assets/Logo/Logo-Full-Light.png";
-import { Link } from "react-router-dom";
+import { Link, matchPath } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 const Navbar = () => {
+  // creating a function for mark YELLOW where on nav which page are watching
+  const location = useLocation();
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname);
+  };
+
   return (
     <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         <Link to={"/"}>
-          <img src={Logo} alt="Logo" />
+          <img src={Logo} alt="Logo" width={160} height={42} loading="lazy" />
         </Link>
 
-        <div className="flex flex-row">
-          {NavbarLinks.map((ele, index) => {
-            return (
-              <div key={index}>
-                <Link to={ele.path}>{ele.title}</Link>
-              </div>
-            );
-          })}
-        </div>
+        {/* nav links */}
+        <nav>
+          <ul className="flex gap-x-6 text-richblack-25">
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <div></div>
+                ) : (
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        {/* Login/signUp dasboard */}
+        <div className="flex gap-x-4 items-center"></div>
+
         <div className="flex flex-row"></div>
       </div>
     </div>
